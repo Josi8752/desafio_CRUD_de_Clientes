23d75f3e7b5a4b6a4050d1358b6791bbfe5a09fe
Josiane Silva <josianesouza8752@gmail.com>
2022-12-13 19:01:52 -0300

    tb_client

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Josiane Ramos", "08254360534", 5000.00, "1994-12-04", 1);
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Mateus Nobrega", "08239421645", 2000.00, 22-09-1998, 3);
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Paula Gabriela", "08276430567", 3000.00, 01-01-1997, 2);
+
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Luana Silva", "04090978457", 4000.00, 28-02-1996, 0);
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Lucas Pereira", "05059421634", 2500.00, 15-03-1988, 2);
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Valdivino Fernandes", "14039823477", 4500.00, 12-12-1974, 2);
+
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Rosana Bezerra", "14034567800", 3500.00, 30-12-1996,2);
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Maria Alice", "05021690960", 2000.00, 24-11-1998, 1);
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Jordanne Aparecida", "07654312892", 5000.00, 09-08-1967, 3);
+INSERT INTO tb_client(name, cpf, income, birthDate, children) VALUES ("Emanuel Lopes", "04012398788", 1500.00, 12-10-1995, 0);
+
